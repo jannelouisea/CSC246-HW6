399004e088140842aefe1fb0f5eb86cc55009dce
jannelouisea <janne.louise.a@gmail.com>
2017-11-30 17:25:29 -0500

    Finisheddddd

--- server.c
+++ server.c
@@ -12,17 +12,6 @@
 
 int port = 0;
 
-/*
-struct client {
-    char * id;
-    int in_use;
-};
-
-struct client clients[NUM_OF_CLIENTS];
-
-int avail_clients = 0;
- */
-
 struct city {
     char * abv;
     int curr_hour;
@@ -49,7 +38,6 @@ int get_curr_hour() {
     currtime = tv.tv_sec;
     currtm = localtime(&currtime);
     currhour = currtm->tm_hour;
-    printf("Curr hour %d\n", currhour);
 
     return currhour;
 }
@@ -58,7 +46,6 @@ void set_cities() {
     int currhour = get_curr_hour();
     for (int i = 0; i < NUM_OF_CITIES; i++) {
         cities[i].curr_hour = currhour;
-        printf("abv %s curr_hour %d sum %d num_tms %d tmp %d\n", cities[i].abv, cities[i].curr_hour, cities[i].sum_tmps, cities[i].num_tms_curr_hour, cities[i].hour_avg_temp);
     }
 }
 
@@ -74,8 +61,6 @@ char * process_message(char * msg) {
         }
         j++;
     }
-
-    printf("colon_count %d\n", colon_count);
 
     int i = 0;
     char ** tokens = malloc((colon_count + 1) * sizeof(char *));
@@ -94,14 +79,7 @@ char * process_message(char * msg) {
             token = strtok(0, ":");
         }
 
-        printf("Tokens:\n");
-        for (int d = 0; d < (colon_count + 1); d++) {
-            printf("%s ", tokens[d]);
-        }
-        printf("\n");
-
         if (colon_count == 0 && (strcmp(tokens[0], "s") == 0 || strcmp(tokens[0], "S") == 0)) {
-            printf("Show message\n");
             // send client temp
             strcpy(reply, "");
 
@@ -115,38 +93,8 @@ char * process_message(char * msg) {
                 strcat(reply, " ");
             }
         } else if (colon_count == 1 && strcmp(tokens[0], "G") == 0) {
-            printf("Register message\n");
             strcpy(reply, tokens[1]);
             strcat(reply, " registered with server");
-
-            // check id client already exists
-            /*
-            int id_in_use = 0;
-            int new_client_idx = -1;
-            for (int i = 0; i < avail_clients; i++) {
-                printf("i %d", i);
-                if (strcmp(clients[i].id, tokens[1]) == 0) {
-                    if (clients[i].in_use == 0) {
-                        new_client_idx = i;
-                        clients[i].in_use = 1;
-                    } else {
-                        id_in_use = 1;
-                    }
-                }
-            }
-            printf("Here");
-
-            strcpy(reply, tokens[1]);
-            if (!id_in_use) {
-                if (new_client_idx < 0) {       // client id is not in clients
-                    strcpy(clients[avail_clients].id, tokens[1]);
-                    clients[avail_clients].in_use = 1;
-                }
-                strcat(reply, " registered with server");
-            } else {
-                strcat(reply, " id is already in use");
-            }
-             */
         } else  if (colon_count == 3 && (strcmp(tokens[0], "r") == 0 || strcmp(tokens[0], "R") == 0)) {
             // Check the city code
             int city_idx = -1;
@@ -159,15 +107,12 @@ char * process_message(char * msg) {
             if (city_idx < 0) {
                 strcpy(reply, "Error city code!");
             } else {
-                // printf("Updating %s temp\n", cities[city_idx].abv);
-
                 // Get the current Raleigh hour
                 int currhour = get_curr_hour();
 
                 if (atoi(tokens[2]) != currhour) {
                     strcpy(reply, "Error hour timestamp!");
                 } else {
-                    // printf("Updating city temp\n");
                     // check if tmp is in range
                     int temp = atoi(tokens[3]);
                     if (temp < 10 || temp > 99) {         // tmp must be a 2 digit, non neg #
@@ -195,10 +140,8 @@ char * process_message(char * msg) {
         }
 
         free(token);
-        printf("\n");
     } else {
-        printf("Error: cannot make tokens from message\n");
-        // ???
+        // printf("Error: cannot make tokens from message\n");
     }
 
     free(tokens);
@@ -209,8 +152,6 @@ char * process_message(char * msg) {
 int main(int argc, char *argv[]) {
 
     set_cities();
-
-    // clients = malloc(NUM_OF_CLIENTS * sizeof(struct client));
 
     // Get the port number
     if (argc == 2) {
@@ -228,7 +169,6 @@ int main(int argc, char *argv[]) {
         // Receiving message from client
         char * message = malloc(BUFFER_SIZE * sizeof(char));
         int rc = UDP_Read(sd, &addr, message, BUFFER_SIZE);
-        printf("Server got message %s\n", message);
 
         int len = strlen(message);
         message[len - 1] = '\0';
@@ -239,10 +179,8 @@ int main(int argc, char *argv[]) {
             rc = UDP_Write(sd, &addr, reply, BUFFER_SIZE);
         }
 
-        // Free message?
+        free(message);
     }
-
-    // free(clients);
 
     return 0;
 }
